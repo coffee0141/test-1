e1e1f028ec76ec01d138bbf7e633b930120ad51b
Haru Motoyama <s23c1139pj@s.chibakoudai.jp>
2024-11-20 10:46:38 +0900

    Add a command

---
+++ ros2_ws/src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    maintainer='Haru Motoyama',
+    maintainer_email='s23c1139pj@s.chibakoudai.jp',
+    
+    description='a package for practice',
+    license='BSD-3-Clause',
