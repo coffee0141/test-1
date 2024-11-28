c55025dc3d236c4d9c8eb4b63c57e8dcbb1dd22a
Haru Motoyama <s23c1139pj@s.chibakoudai.jp>
2024-11-28 11:58:46 +0900

    Add

--- ros2_ws/build/mypkg/prefix_override/sitecustomize.py
+++ ros2_ws/build/mypkg/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/haru/ros2_ws/install/mypkg'
+    sys.prefix = sys.exec_prefix = '/home/haru/test-1/ros2_ws/install/mypkg'
--- ros2_ws/install/mypkg/lib/python3.8/site-packages/mypkg/talker.py
+++ ros2_ws/install/mypkg/lib/python3.8/site-packages/mypkg/talker.py
@@ -1,20 +1,20 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-
+from person_msgs.msg import Person 
+ 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10) #変更
 n = 0
-
-def cb():       
-     global n       
-     msg = Int16()  
-     msg.data = n   
-     pub.publish(msg)        
+ 
+ 
+def cb():
+     global n
+     msg = Person()         #送信するデータの型を変更
+     msg.name = "本山暖"  #msgファイルに書いた「name」
+     msg.age = n            #msgファイルに書いた「age」
+     pub.publish(msg)
      n += 1
-
-
 def main():
-     node.create_timer(0.5, cb) 
-     rclpy.spin(node) 
+     node.create_timer(0.5, cb)
+     rclpy.spin(node)
